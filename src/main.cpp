1f870ec9a365284c11775c80752a5deafbf4a278
Ryan Machado <rmachado.ctr@kr.af.mil>
2022-01-21 18:12:03 -0500

    .

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
 
 int main() {
 
-	std::cout
-		<< "Hello, world!\n"
-		<< std::endl;
+//	std::cout
+//		<< "Hello, world!\n"
+//		<< std::endl;
 
-    system("osascript \"/usr/local/opt/foo.applescript\"");
+    system("osascript \"/usr/local/opt/foo.applescript\" > /dev/null");
 
 	return 0;
 
