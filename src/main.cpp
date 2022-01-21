e77b2f658c431caad60392d518eefe9e70012b1d
Ryan Machado <rmachado.ctr@kr.af.mil>
2022-01-21 17:11:28 -0500

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include <unistd.h>
 
-char version[] = "9.0.0";
-
 int main() {
-
-	std::string username = getlogin();
 
 	std::cout
 		<< "Hello, world!\n"
-		<< "Version " << version
 		<< std::endl;
+
+    system("osascript \"/usr/local/opt/foo.applescript\"");
 
 	return 0;
 
